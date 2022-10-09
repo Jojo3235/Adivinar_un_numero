fe3933e196f4b73f6dcfbbf626d8585575150b10
Jojo3235 <joralmand@gmail.com>
2022-10-10 00:31:12 +0200

    Más pruebas

--- pruebas.py
+++ pruebas.py
@@ -1,11 +1,27 @@
 import random as rnd
-i=0
-numero= rnd.randint(0, 100)
-print(numero)
-while i<1:
-    elección=int(input("Introduce un numero: "))
-    if elección==numero:
-        i+=1
-        print("¡Has acertado el número!")
-else:
-    pass
+from timeit import repeat
+def acertar(numero):
+    print(numero)
+    tries=0
+    while True:
+        elección=int(input("Introduce un numero: "))
+        tries+=1
+        if elección==numero:
+            print("¡Has acertado el número!")
+            if tries!=1:
+                print("Número de intentos", tries)
+                break
+            else:
+                print("Número de intentos: 1")
+                break
+        else:
+            if elección < numero:
+                print("Prueba un número más grande")
+            else:
+                print("Prueba un número más pequeño")
+
+def limites(first_bound,second_bound):
+    if first_bound<second_bound:
+        return rnd.randint(first_bound,second_bound)
+    elif first_bound>second_bound:
+        return rnd.randint(second_bound,first_bound)
