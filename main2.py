60b238c8d777db9cb9d52b2c0e513053ff39673f
Jojo3235 <joralmand@gmail.com>
2022-10-17 12:22:24 +0200

    comentarios 1

--- main2.py
+++ main2.py
@@ -3,6 +3,8 @@ lvl 1 -> 0-100  10 atts
 lvl 2 -> 0-1.000   17 atts
 lvl 3 -> 0-1.000.000    34 atts
 lvl 4 -> 0 1.000.000.000.000     60 atts
+
+Para las mejores puntuaciones guardar solo la de los niveles
 """
 
 def dificultad(dif_1,dif_2):
@@ -16,30 +18,3 @@ def dificultad(dif_1,dif_2):
     else:
         print("No se ha introducido un número válido")
 
-def atts_max(atts,max):
-    while True:
-        if atts>=max:
-            print("Has perdido")
-            break
-        else:
-            pass
-
-atts=00
-if dificultad(1,4)==1:
-    lim1=0
-    lim2=100
-    atts_max(atts,7)
-elif dificultad(1,4)==2:
-    lim1=0
-    lim2=1000
-    atts_max(atts,12)
-elif dificultad(1,4)==3:
-    lim1=0
-    lim2=1000000
-    atts_max(atts,24)
-elif dificultad(1,4)==4:
-    lim1=0
-    lim2=1000000000000
-    atts_max(atts,40)
-else:
-    print("No existe esa dificultad")
